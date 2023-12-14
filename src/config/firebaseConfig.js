d592f6f1d9186176c80e5569bdcb115f25e8a04e
SaTyle <zxcvwifi@gmail.com>
2023-12-14 21:49:48 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWKaRuj0QAtnIgJ2mWpelmsUPCklFkt2A",
+  authDomain: "crypto-analyzer-loginwatchlist.firebaseapp.com",
+  projectId: "crypto-analyzer-loginwatchlist",
+  storageBucket: "crypto-analyzer-loginwatchlist.appspot.com",
+  messagingSenderId: "890401405091",
+  appId: "1:890401405091:web:f84a7a0cf2831673c4a716"
+};
+
+
+export default firebaseConfig;
